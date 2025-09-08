b050b2a96d192c9f9beb98ac3375d609b6b91425
Hoanghieu <hoanghieu.nd.4820@gmail.com>
2025-09-08 14:27:50 +0700

    commit 8/10

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+
 const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+// Cho phép Express phục vụ file tĩnh (CSS, JS, ảnh...)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Route trả về file index.html
 app.get('/', (req, res) => {
-  res.send('Hello World express!')
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+// Chạy server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`App listening at http://localhost:${port}`);
+});
 
